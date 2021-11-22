8d25a3d95353dbba1e9c2f6516c66ad8ff3298f1
Tom <tom.cohen1996@gmail.com>
2021-11-21 20:04:54 -0500

    throw

--- ComputerAI.py
+++ ComputerAI.py
@@ -29,6 +29,7 @@ class ComputerAI(BaseAI):
         return new_pos
 
     def getTrap(self, grid : Grid):
+        """Get the trap move of the player"""
         
         # find all available cells in the grid
         available_cells = grid.getAvailableCells()
@@ -36,39 +37,6 @@ class ComputerAI(BaseAI):
         # find all available cells
         trap = random.choice(available_cells) if available_cells else None
 
-        return self.throw(grid, trap)
-
-    def throw(self, grid : Grid, intended_position : tuple) :
-        '''
-        Description
-        ----------
-        Function returns the coordinates in which the trap lands, given an intended location.
-
-        Parameters
-        ----------
-
-        grid : current game Grid
-
-        intended position : the (x,y) coordinates to which the player intends to throw the trap to.
-        '''
-        # find neighboring cells
-        neighbors = grid.get_neighbors(intended_position)
-
-        n = len(neighbors)
-        
-        probs = np.ones(1 + n)
-        
-        # compute probability of success, p
-        p = 1 - 0.05*(manhattan_distance(self.pos, intended_position) - 1)
-
-        probs[0] = p
-
-        probs[1:] = np.ones(len(neighbors)) * ((1-p)/n)
-
-        return np.random.choice(np.concatenate(([intended_position], neighbors)), p = probs)
-
-
-def manhattan_distance(position, target):
-    return np.abs(target[0] - position[0]) + np.abs(target[1] - position[1])
+        return trap
 
 
